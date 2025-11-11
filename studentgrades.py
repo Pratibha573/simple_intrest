bd68faeb0f1974d5fbd66cd8deeb0dc81078a22a
Pratibha <Pratibha573>
2025-11-11 09:47:25 +0530

    save

---
+++ studentgrades.py
@@ -0,0 +1,44 @@
+import sys
+
+if len(sys.argv) == 7:
+    name = sys.argv[1]
+    marks_1 = int(sys.argv[2])
+    marks_2 = int(sys.argv[3])
+    marks_3 = int(sys.argv[4])
+    marks_4 = int(sys.argv[5])
+    marks_5 = int(sys.argv[6])
+else:
+    name = "xyz"
+    marks_1 = 50
+    marks_2 = 70
+    marks_3 = 85
+    marks_4 = 90
+    marks_5 = 85
+
+total = marks_1 + marks_2 + marks_3 + marks_4 + marks_5
+avg = total / 5
+
+if avg >= 90:
+    grade = "A"
+elif avg >= 80:
+    grade = "B"
+elif avg >= 70:
+    grade = "C"
+elif avg >= 60:
+    grade = "D"
+elif avg >= 50:
+    grade = "E"
+elif avg >= 40:
+    grade = "F"
+else:
+    grade = "Fail"
+
+print("File:", name)
+print("Eng: ",marks_1)
+print("Math: ",marks_2)
+print("DevOps: ",marks_3)
+print("SSW: ",marks_4)
+print("SE: ",marks_5)
+print("Total Marks:",total)
+print("Average: ",avg)
+print("Grade: ",grade)
